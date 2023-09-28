100229ad4828a0504a56505d1a64936ae4fdb5c3
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-09-28 15:57:37 +0200

    Paso 7

--- app/inicio.php
+++ app/inicio.php
@@ -9,9 +9,21 @@ require_once "libs/MySQLdb.php";
 
 class Application
 {
-    function __construct()
-    {
-        $db = MySQLdb::getInstance()->getDatabase();
-        print "Conexión extablecida";
+    public function __construct()
+    {;
+        $url = $this->separarURL();
+        var_dump($url);
     }
+
+    public function separarURL()
+    {
+        if ($_SERVER['REQUEST_URI'] != '/') {
+            $url = trim($_SERVER['REQUEST_URI'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            return $url;
+        }
+    }
+
 }
