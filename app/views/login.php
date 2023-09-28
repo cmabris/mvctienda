a6d3f54c64b0865c02ba90bcf59d8c31fd9618c1
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-09-28 21:16:02 +0200

    Fin paso 14

--- app/views/login.php
+++ app/views/login.php
@@ -6,25 +6,42 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </head>
 <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a href="index.php" class="navbar-brand">Tienda</a>
+    </nav>
+    <div class="container-fluid">
+        <div class="row content">
+            <div class="col-sm-2">
 
-<form action="">
-    <table>
-        <tr>
-            <td>Usuario: </td>
-            <td><input type="text" name="user"></td>
-        </tr>
-        <tr>
-            <td>Clave de acceso: </td>
-            <td><input type="password" name="password"></td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td><input type="submit" value="Enviar"></td>
-        </tr>
-    </table>
-</form>
+            </div>
+            <div class="col-sm-8">
+                <form action="">
+                    <table>
+                        <tr>
+                            <td>Usuario: </td>
+                            <td><input type="text" name="user"></td>
+                        </tr>
+                        <tr>
+                            <td>Clave de acceso: </td>
+                            <td><input type="password" name="password"></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td><input type="submit" value="Enviar"></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+            <div class="col-sm-2">
+
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
