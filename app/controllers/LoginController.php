a466052ac8b0e47b65c41b28e1c3143dd45dd37b
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-10-02 17:10:37 +0200

    Fin paso 19

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -21,12 +21,75 @@ class LoginController extends Controller
 
     public function registro()
     {
-        $data = [
-            'title' => 'Registro',
-            'menu' => false,
-        ];
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $this->view('register', $data);
+            //Validar datos del formulario
+            $firstName = $_POST['first_name'] ?? '';
+            $lastName1 = $_POST['last_name_1'] ?? '';
+            $lastName2 = $_POST['last_name_2'] ?? '';
+            $email = $_POST['email'] ?? '';
+            $password1 = $_POST['password1'] ?? '';
+            $password2 = $_POST['password1'] ?? '';
+            $address = $_POST['address'] ?? '';
+            $city = $_POST['city'] ?? '';
+            $state = $_POST['state'] ?? '';
+            $postcode = $_POST['postcode'] ?? '';
+            $country = $_POST['country'] ?? '';
+
+            if ($firstName == '') {
+                array_push($errors, 'El nombre es requerido');
+            } else {
+                //Si está relleno realizar otras comprobaciones
+            }
+            if ($lastName1 == '') {
+                array_push($errors, 'El primer apellido es requerido');
+            }
+            if ($email == '') {
+                array_push($errors, 'El email es requerido');
+            }
+            if ($password1 == '') {
+                array_push($errors, 'La contraseña es requerida');
+            }
+            if ($password2 == '') {
+                array_push($errors, 'La repetición de contraseña es requerida');
+            }
+            if ($address == '') {
+                array_push($errors, 'La dirección es requerida');
+            }
+            if ($city == '') {
+                array_push($errors, 'La ciudad es requerida');
+            }
+            if ($state == '') {
+                array_push($errors, 'La provincia es requerida');
+            }
+            if ($postcode == '') {
+                array_push($errors, 'El código postal es requerido');
+            }
+            if ($country == '') {
+                array_push($errors, 'El país es requerido');
+            }
+            if ($password1 != $password2) {
+                array_push($errors, 'Ambas claves deben ser iguales');
+            }
+            if ( ! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                array_push($errors, 'El correo electrónico no es válido');
+            }
+            if (count($errors) == 0) {
+                echo "Pasamos a dar de alta los datos en la BD";
+            } else {
+                var_dump($errors);
+            }
+
+        } else {
+
+            $data = [
+                'title' => 'Registro',
+                'menu' => false,
+            ];
+
+            $this->view('register', $data);
+
+        }
     }
 
     public function olvido()
