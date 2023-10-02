e453c929183e7e6a86ca9a7d12a81c58720ce192
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-10-02 15:48:01 +0200

    Fin paso 15

--- app/views/login.php
+++ app/views/login.php
@@ -20,22 +20,40 @@
 
             </div>
             <div class="col-sm-8">
-                <form action="">
-                    <table>
-                        <tr>
-                            <td>Usuario: </td>
-                            <td><input type="text" name="user"></td>
-                        </tr>
-                        <tr>
-                            <td>Clave de acceso: </td>
-                            <td><input type="password" name="password"></td>
-                        </tr>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <td><input type="submit" value="Enviar"></td>
-                        </tr>
-                    </table>
-                </form>
+                <div class="card p-4 bg-light">
+                    <div class="card-header">
+                        <h1 class="text-center">Login</h1>
+                    </div>
+                    <div class="card-body">
+                        <form action="login/verifyUser" method="POST">
+                            <div class="form-group text-left mb-2">
+                                <label for="user">Usuario:</label>
+                                <input type="text" name="user" class="form-control">
+                            </div>
+                            <div class="form-group text-left mb-2">
+                                <label for="password">Contraseña:</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                            <div class="form-group text-left mb-2">
+                                <input type="checkbox" name="remember">
+                                <label for="remember">Recordar</label>
+                            </div>
+                            <div class="form-group text-left">
+                                <input type="submit" value="Enviar" class="btn btn-success">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <a href="login/alta" class="btn btn-info">Nuevo usuario</a>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="login/olvido" class="btn btn-info">¿No recuerdas la contraseña?</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-2">
 
