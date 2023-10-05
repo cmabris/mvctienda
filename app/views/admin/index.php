cc5c5c76dfcabf81226f0363c43546d4bda375cd
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-10-05 16:04:59 +0200

    Fin paso 35

---
+++ app/views/admin/index.php
@@ -0,0 +1,28 @@
+<?php include_once (dirname(__DIR__). ROOT . 'header.php') ?>
+<div class="card p-4 bg-light">
+    <div class="card-header">
+        <h1 class="text-center">Módulo de Administración</h1>
+    </div>
+    <div class="card-body">
+        <form action="<?= ROOT ?>admin/verifyUser" method="POST">
+            <div class="form-group text-left mb-2">
+                <label for="user">Usuario:</label>
+                <input type="text" name="user" class="form-control"
+                       placeholder="Escribe tu correo electrónico"
+                       value="<?= $data['data']['user'] ?? '' ?>">
+            </div>
+            <div class="form-group text-left mb-2">
+                <label for="password">Contraseña:</label>
+                <input type="password" name="password" class="form-control"
+                       value="<?= $data['data']['password'] ?? '' ?>">
+            </div>
+            <div class="form-group text-left">
+                <input type="submit" value="Enviar" class="btn btn-success">
+            </div>
+        </form>
+    </div>
+</div>
+
+
+<?php include_once (dirname(__DIR__). ROOT . 'footer.php') ?>
+
