a0741d0510707f87aa57532d230d78d979a63696
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-10-16 17:54:49 +0200

    Fin paso 73

--- app/controllers/AdminProductController.php
+++ app/controllers/AdminProductController.php
@@ -182,7 +182,152 @@ class AdminProductController extends Controller
 
     public function update($id)
     {
+        $errors = [];
+        $typeConfig = $this->model->getConfig('productType');
+        $statusConfig = $this->model->getConfig('productStatus');
+        $catalogue = $this->model->getCatalogue();
 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            //Recibimos la información
+            $type = $_POST['type'] ?? '';
+            $name = Validate::text($_POST['name'] ?? '');
+            $description = Validate::text($_POST['description'] ?? '');
+            $price = Validate::number($_POST['price'] ?? '');
+            $discount = Validate::number($_POST['discount'] ?? '0');
+            $send = Validate::number($_POST['envio'] ?? '0');
+            $image = Validate::file($_FILES['image']['name']);
+            $published = $_POST['published'] ?? '';
+            $relation1 = $_POST['relation1'] != '' ? $_POST['relation1'] : 0;
+            $relation2 = $_POST['relation2'] != '' ? $_POST['relation2'] : 0;
+            $relation3 = $_POST['relation3'] != '' ? $_POST['relation3'] : 0;
+            $mostSold = isset($_POST['mostSold']) ? '1' : '0';
+            $new = isset($_POST['new']) ? '1' : '0';
+            $status = $_POST['status'] ?? '';
+            //Books
+            $author = Validate::text($_POST['author'] ?? '');
+            $publisher = Validate::text($_POST['publisher'] ?? '');
+            $pages = Validate::number($_POST['pages'] ?? '');
+            //Courses
+            $people = Validate::text($_POST['people'] ?? '');
+            $objetives = Validate::text($_POST['objetives'] ?? '');
+            $necesites = Validate::text($_POST['necesites'] ?? '');
+
+            //Validamos la información
+            if (empty($name)) {
+                array_push($errors, 'El nombre del producto es requerido');
+            }
+            if (empty($description)) {
+                array_push($errors, 'La descripción del producto es requerida');
+            }
+            if ( ! is_numeric($price)) {
+                array_push($errors, 'El precio del producto debe ser un número');
+            }
+            if ( ! is_numeric($discount)) {
+                array_push($errors, 'El descuento del producto debe ser un número');
+            }
+            if ( ! is_numeric($send)) {
+                array_push($errors, 'Los gastos de envío del producto deben ser un número');
+            }
+            if (is_numeric($price) && is_numeric($discount) && $price < $discount) {
+                array_push($errors, 'El descuento no puede ser mayor que el precio');
+            }
+            if ( ! is_numeric($pages)) {
+                $pages = 0;
+            }
+            if ($type == 1) {
+                if (empty($people)) {
+                    array_push($errors, 'El público objetivo es requerido');
+                }
+                if (empty($objetives)) {
+                    array_push($errors, 'Los objetivos del curso son requeridos');
+                }
+                if (empty($necesites)) {
+                    array_push($errors, 'Los requisitos del curso son necesarios');
+                }
+            } elseif ($type == 2) {
+                if (empty($author)) {
+                    array_push($errors, 'El autor del libro es requerido');
+                }
+                if (empty($publisher)) {
+                    array_push($errors, 'La editorial del libro es requerida');
+                }
+            } else {
+                array_push($errors, 'Debes seleccionar un tipo válido');
+            }
+            if ( ! Validate::date($published) ) {
+                array_push($errors, 'La fecha o su formato no son correctos');
+            } elseif (Validate::dateDif($published)) {
+                array_push($errors, 'La fecha de publicación no puede ser posterior a hoy');
+            }
+
+            if ($image) {
+                if (Validate::imageFile($_FILES['image']['tmp_name'])) {
+                    //Comenzamos a tratar la imagen una vez validad
+                    $image = strtolower($image);
+
+                    if (is_uploaded_file($_FILES['image']['tmp_name'])) {
+                        move_uploaded_file($_FILES['image']['tmp_name'], 'img' . ROOT . $image);
+                        Validate::resizeImage($image, 240);
+                    } else {
+                        array_push($errors, 'Error al subir la imagen');
+                    }
+                } else {
+                    array_push($errors, 'El formato de imagen no es aceptado');
+                }
+            }
+
+            //Creamos el array de datos
+            $dataForm = [
+                'id' => $id,
+                'type' => $type,
+                'name' => $name,
+                'description' => $description,
+                'price' => $price,
+                'discount' => $discount,
+                'send' => $send,
+                'image' => $image,
+                'published' => $published,
+                'author' => $author,
+                'publisher' => $publisher,
+                'pages' => $pages,
+                'people' => $people,
+                'objetives' => $objetives,
+                'necesites' => $necesites,
+                'mostSold' => $mostSold,
+                'new' => $new,
+                'relation1' => $relation1,
+                'relation2' => $relation2,
+                'relation3' => $relation3,
+                'status' => $status,
+            ];
+
+            if (empty($errors)) {
+                //Enviar la información al modelo
+
+                if ( ! $this->model->updateProduct($dataForm)) {
+                    //Redirigimos al index de productos
+                    header('location:' . ROOT . 'adminProduct');
+                }
+
+                array_push($errors, 'Se ha producido un error durante la actualización en la BD');
+            }
+        }
+
+        $product = $this->model->getProductById($id);
+
+        $data = [
+            'title' => 'Administración de Productos - Alta',
+            'menu' => false,
+            'admin' => true,
+            'errors' => $errors,
+            'type' => $typeConfig,
+            'status' => $statusConfig,
+            'catalogue' => $catalogue,
+            'product' => $product,
+        ];
+
+        $this->view('admin/products/update', $data);
     }
 
     public function delete($id)
---
+++ app/views/admin/products/update.php
@@ -0,0 +1,204 @@
+<?php include_once (VIEWS . 'header.php') ?>
+<script src="https://cdn.ckeditor.com/ckeditor5/40.0.0/classic/ckeditor.js"></script>
+<script src="<?= ROOT ?>js/adminCreateProduct.js"></script>
+<?php if(isset($data['errors']) && count($data['errors']) > 0): ?>
+    <div class="alert alert-danger mt-3">
+        <?php foreach ($data['errors'] as $value): ?>
+            <strong><?= $value ?></strong><br>
+        <?php endforeach; ?>
+    </div>
+<?php endif ?>
+<div class="card p-4 bg-light">
+    <div class="card-header">
+        <h1 class="text-center">Alta de un producto</h1>
+    </div>
+    <div class="card-body">
+        <form action="<?= ROOT ?>adminProduct/update/<?= $data['product']->id ?>" method="POST" enctype="multipart/form-data">
+            <div class="form-control text-left">
+                <label for="type">Tipo de producto</label>
+                <select name="type" id="type" class="form-control">
+                    <option value="">Selecciona el tipo de producto</option>
+                    <?php foreach ($data['type'] as $type): ?>
+                        <option value="<?= $type->value ?>"
+                            <?= isset($data['product']->type) && $data['product']->type == $type->value ? 'selected' : '' ?>>
+                            <?= $type->description ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <div class="form-group text-left mt-4">
+                <label for="name">Nombre:</label>
+                <input type="text" name="name" id="name"
+                       class="form-control" required
+                       placeholder="Escribe el nombre del producto"
+                       value="<?= $data['product']->name ?? '' ?>">
+            </div>
+            <div class="form-group text-left mt-4">
+                <label for="name">Descripción:</label>
+                <textarea name="description" id="editor"><?= $data['product']->description ?? '' ?></textarea>
+            </div>
+            <div id="book">
+                <div class="form-group text-left">
+                    <label for="author">Autor</label>
+                    <input type="text" name="author" id="author"
+                           class="form-control"
+                           placeholder="Escribe el autor del libro"
+                           value="<?= $data['product']->author ?? '' ?>">
+                </div>
+                <div class="form-group text-left">
+                    <label for="publisher">Editorial</label>
+                    <input type="text" name="publisher" id="publisher"
+                           class="form-control"
+                           placeholder="Escribe la editorial del libro"
+                           value="<?= $data['product']->publisher ?? '' ?>">
+                </div>
+                <div class="form-group text-left">
+                    <label for="pages">Páginas</label>
+                    <input type="text" name="pages" id="pages"
+                           class="form-control"
+                           placeholder="Escribe el número de páginas del libro"
+                           value="<?= $data['product']->pages ?? '' ?>">
+                </div>
+            </div>
+            <div id="course">
+                <div class="form-group text-left">
+                    <label for="people">Público objetivo</label>
+                    <input type="text" name="people" id="people"
+                           class="form-control"
+                           placeholder="Escribe el público objetivo del curso"
+                           value="<?= $data['product']->people ?? '' ?>">
+                </div>
+                <div class="form-group text-left">
+                    <label for="objetives">Objetivos</label>
+                    <input type="text" name="objetives" id="objetives"
+                           class="form-control"
+                           placeholder="Escribe los objetivos del curso"
+                           value="<?= $data['product']->objetives ?? '' ?>">
+                </div>
+                <div class="form-group text-left">
+                    <label for="necesites">Conocimientos necesarios previos</label>
+                    <input type="text" name="necesites" id="necesites"
+                           class="form-control"
+                           placeholder="Escribe el número de páginas del libro"
+                           value="<?= $data['product']->necesites ?? '' ?>">
+                </div>
+            </div>
+            <div class="form-group text-left">
+                <label for="price">Precio</label>
+                <input type="text" name="price" id="price"
+                       pattern="^(\d|-)?(\d|,)*\.?\d*$"
+                       class="form-control" required
+                       placeholder="Escribe el precio del producto sin comas ni símbolos"
+                       value="<?= $data['product']->price ?? '' ?>">
+            </div>
+            <div class="form-group text-left">
+                <label for="discount">Descuento del producto</label>
+                <input type="text" name="discount" id="discount"
+                       pattern="^(\d|-)?(\d|,)*\.?\d*$"
+                       class="form-control" required
+                       placeholder="Escribe el descuento del producto sin comas ni símbolos"
+                       value="<?= $data['product']->discount ?? '' ?>">
+            </div>
+            <div class="form-group text-left">
+                <label for="envio">Coste del envío del producto</label>
+                <input type="text" name="envio" id="envio"
+                       pattern="^(\d|-)?(\d|,)*\.?\d*$"
+                       class="form-control" required
+                       placeholder="Escribe el coste del envío del producto sin comas ni símbolos"
+                       value="<?= $data['product']->send ?? '' ?>">
+            </div>
+            <div class="form-group text-left">
+                <label for="image">Imagen del producto</label>
+                <input type="file" name="image" id="image"
+                       class="form-control"
+                       accept="image/jpeg,image/x-png,image/gif">
+            </div>
+            <div class="form-group text-left">
+                <label for="published">Fecha del producto</label>
+                <input type="date" name="published" id="published"
+                       class="form-control"
+                       placeholder="Fecha de creación o publicación del producto (AAAA-MM-DD)"
+                       value="<?= $data['product']->published ?? '' ?>">
+            </div>
+            <div class="form-group text-left">
+                <label for="relation1">Producto relacionado</label>
+                <select name="relation1" id="relation1" class="form-control">
+                    <option value="">Selecciona un producto relacionado</option>
+                    <?php foreach ($data['catalogue'] as $item): ?>
+                        <option value="<?= $item->id ?>"
+                            <?= isset($data['product']->relation1) && $data['product']->relation1 == $item->id ? 'selected' : '' ?>>
+                            <?= $item->name ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <div class="form-group text-left">
+                <label for="relation2">Producto relacionado</label>
+                <select name="relation2" id="relation2" class="form-control">
+                    <option value="">Selecciona un producto relacionado</option>
+                    <?php foreach ($data['catalogue'] as $item): ?>
+                        <option value="<?= $item->id ?>"
+                            <?= isset($data['product']->relation2) && $data['product']->relation2 == $item->id ? 'selected' : '' ?>>
+                            <?= $item->name ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <div class="form-group text-left">
+                <label for="relation3">Producto relacionado</label>
+                <select name="relation3" id="relation3" class="form-control">
+                    <option value="">Selecciona un producto relacionado</option>
+                    <?php foreach ($data['catalogue'] as $item): ?>
+                        <option value="<?= $item->id ?>"
+                            <?= isset($data['product']->relation3) && $data['product']->relation3 == $item->id ? 'selected' : '' ?>>
+                            <?= $item->name ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <div class="form-group text-left">
+                <label for="status">Estado del producto</label>
+                <select name="status" id="status" class="form-control">
+                    <option value="void">Selecciona el estado del producto</option>
+                    <?php foreach ($data['status'] as $status): ?>
+                        <option value="<?= $status->value ?>"
+                            <?= isset($data['product']->status) && $data['product']->status == $status->value ? 'selected' : '' ?>>
+                            <?= $status->description ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <div class="form-check text-left">
+                <input type="checkbox" name="mostSold" id="mostSold"
+                    <?= isset($data['product']->mostSold) && $data['product']->mostSold == '1' ? 'checked' : '' ?>
+                       class="form-check-input">
+                <label for="mostSold" class="form-check-label">
+                    Producto más vendido
+                </label>
+            </div>
+            <div class="form-check text-left">
+                <input type="checkbox" name="new" id="new"
+                    <?= isset($data['product']->new) && $data['product']->new == '1' ? 'checked' : '' ?>
+                       class="form-check-input">
+                <label for="new" class="form-check-label">
+                    Producto nuevo
+                </label>
+            </div>
+            <div class="form-group text-left">
+                <input type="submit" value="Crear producto" class="btn btn-success">
+                <a href="<?= ROOT ?>adminproduct" class="btn btn-info">Regresar</a>
+            </div>
+        </form>
+    </div>
+</div>
+<?php include_once (VIEWS . 'footer.php') ?>
+<script>
+    ClassicEditor
+        .create( document.querySelector( '#editor' ) )
+        .then( editor => {
+            console.log( editor );
+        } )
+        .catch( error => {
+            console.error( error );
+        } );
+</script>
