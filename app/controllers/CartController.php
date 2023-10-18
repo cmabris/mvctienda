7f8085080e09e1b7029b0ec82849cb9dfe1ba680
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-10-18 20:57:36 +0200

    Fin paso 93

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -9,8 +9,21 @@ class CartController extends Controller
         $this->model = $this->model('Cart');
     }
 
+    public function index()
+    {
+        echo 'Estoy en index de Carrito';
+    }
+
     public function addProduct($product_id, $user_id)
     {
-        var_dump($product_id, $user_id);
+        $errors = [];
+
+        if ( ! $this->model->verifyProduct($product_id, $user_id)) {
+            if ( $this->model->addProduct($product_id, $user_id) == false) {
+                array_push($errors, 'Error al insertar el producto en el carrito');
+            }
+        }
+
+        $this->index();
     }
 }
