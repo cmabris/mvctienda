460713aca2508e6a7b7819caf537fe61145aa1f9
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-10-24 18:52:34 +0200

    Fin paso 108

--- app/views/admin/carts/index.php
+++ app/views/admin/carts/index.php
@@ -14,6 +14,28 @@
                 <th>Total</th>
                 <th>Detalle</th>
             </thead>
+            <?php $cost=0; $discount=0; $send=0; $total=0 ?>
+            <?php foreach($data['data'] as $sale): ?>
+            <tr>
+                <td class="text-center"><?= $sale->user_id ?></td>
+                <td class="text-center"><?= substr($sale->date, 0, 10) ?></td>
+                <td class="text-right"><?= number_format($sale->cost, 2) ?></td>
+                <td class="text-right"><?= number_format($sale->discount, 2) ?></td>
+                <td class="text-right"><?= number_format($sale->send, 2) ?></td>
+                <td class="text-right"><?= number_format($sale->cost - $sale->discount + $sale->send, 2) ?></td>
+                <td><a href="<?= ROOT ?>admincart/show/<?= substr($sale->date, 0, 10) ?>/<?= $sale->user_id ?>" class="btn btn-info">Detalle</a></td>
+            </tr>
+                <?php $cost+=$sale->cost; $discount+=$sale->discount; $send+=$sale->send; $total+=$sale->cost - $sale->discount + $sale->send ?>
+            <?php endforeach ?>
+            <tr>
+                <td></td>
+                <td>Totales:</td>
+                <td><?= number_format($cost, 2) ?></td>
+                <td><?= number_format($discount, 2) ?></td>
+                <td><?= number_format($send, 2) ?></td>
+                <td><?= number_format($total, 2) ?></td>
+                <td></td>
+            </tr>
         </table>
     </div>
 </div>
